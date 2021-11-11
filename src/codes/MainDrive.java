c78868dd7a1da9e1b47a666ce8bd4c5301f3b0cf
clearsky77 <clearsky3264@gmail.com>
2021-11-11 12:31:45 +0900

    6개 숫자 단순 반복으로 입력

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,9 +1,21 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-
+		Scanner myScanner = new Scanner(System.in);
+		
+		int[] myInputNumbers = new int[6];
+		
+		for(int i=0; i<myInputNumbers.length; i++) {
+			System.out.print(i+1+"번째 숫자 입력 : ");
+			int inputNum = myScanner.nextInt();
+			myInputNumbers[i] = inputNum;
+			
+		}
+		
 	}
 
 }
